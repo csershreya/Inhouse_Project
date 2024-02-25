0c508c9d9bcf44591d695e4e231cc5e5f9b0f646
Tanushka109 <tanushkasingh910@gamil.com>
2024-02-25 22:09:44 +0530

    admin-index updated

--- admin-login/server.js
+++ admin-login/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = 3080;
+const port = 3082;
 
 // Middleware
 app.use(bodyParser.json());
