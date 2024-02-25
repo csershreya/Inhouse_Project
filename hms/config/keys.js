0554e206b11691718e72d4d138661210c4ce2fda
Tanushka109 <114823750+Tanushka109@users.noreply.github.com>
2024-02-25 21:13:12 +0530

    Delete hms directory

--- hms/config/keys.js
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./keys_prod");
-} else if (process.env.NODE_ENV === "test"){
-  module.exports = require("./keys_test");
-} else {
-  module.exports = require("./keys_dev");
-}
