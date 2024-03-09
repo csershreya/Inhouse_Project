ca633fe9c2ba421bcdafcca558dacec171d01ec5
k-gupta2518 <you@example.com>
2024-03-10 01:51:37 +0530

    regular

--- student-module/student_server.js
+++ student-module/student_server.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Shreya_29',
+    password: 'aryakhushi1234',
     database: 'shms'
 });
 
